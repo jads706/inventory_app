79481f1fb534bab40892748a03abd7e44f355d44
jads706 <jads706@gmail.com>
2020-10-20 01:45:12 +0000

    Fixed syntax error that was causing issue for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,9 +97,9 @@ Rails.application.configure do
     :address => 'smtp.gmail.com',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV('GMAIL_USERNAME'),
-    :password => ENV('GMAIL_APP_PASSWORD'),
-    # :domain => 'heroku.com',
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_APP_PASSWORD'],
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
 
